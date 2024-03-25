2d4bc45288eb62f91fac8d11d7d43e9574cfdb05
piggy <babymechanic@gmail.com>
2024-03-25 15:37:26 +1100

    putting dependency on iam role to prevent destroy of ecs getting stuck

--- modules/chuck_norris_say/ecs.tf
+++ modules/chuck_norris_say/ecs.tf
@@ -21,7 +21,11 @@ resource "aws_ecs_service" "chuck_norris_app" {
     target_group_arn = aws_lb_target_group.chuck_norris.arn
   }
 
-  depends_on = [aws_lb_listener.http_forward, aws_iam_role_policy_attachment.ecs_task_execution_role]
+  depends_on = [
+    aws_lb_listener.http_forward,
+    aws_iam_role_policy_attachment.ecs_task_execution_role,
+    aws_iam_role.ecs_task_execution_role
+  ]
 
   tags = {
     Environment = var.environment,
